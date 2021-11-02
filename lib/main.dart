fa610bd05b21b2e51beecfe680fa9c0abf997cce
chowdhuryrahulc <84246818+chowdhuryrahulc@users.noreply.github.com>
2021-11-02 14:37:52 +0530

    First

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite_tutorial/presentation/pages/add_category_page.dart';
+import 'package:sqflite_tutorial/presentation/pages/add__contact_page.dart';
+import 'package:sqflite_tutorial/presentation/pages/contacts_page.dart';
+import 'package:sqflite_tutorial/presentation/pages/edit_contact_page.dart';
+import 'package:sqflite_tutorial/presentation/pages/search_contacts.dart';
+import 'package:sqflite_tutorial/presentation/pages/search_contacts_by_category.dart';
+import 'package:sqlwithforiegnkey/pages/add_contactPage.dart';
+import 'package:sqlwithforiegnkey/pages/contact_page.dart';
+import 'package:sqlwithforiegnkey/pages/edit_contactPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/homePage': (context) => ContactsPage(),
+        '/addContactPage': (context) => AddContactPage(),
+        '/edit': (context) => EditContactPage(),
+        '/searchPage': (context) => SearchPage(),
+        '/addCategoryPage': (context) => AddCategoryPage(),
+        '/searchContactsByCategory': (context) => SearchContactsByCategory()
+      },
+      home: ContactsPage(),
+    );
+  }
+}
---
+++ lib/pages/add_contactPage.dart
@@ -0,0 +1,89 @@
+// ignore_for_file: prefer_const_constructors, file_names, prefer_const_constructors_in_immutables
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sqflite_tutorial/data/category_operations.dart';
+import 'package:sqflite_tutorial/data/contact_operations.dart';
+import 'package:sqflite_tutorial/models/contact.dart';
+import 'package:sqflite_tutorial/models/category.dart';
+import 'package:sqflite_tutorial/presentation/widgets/categories_dropdown.dart';
+
+class AddContactPage extends StatefulWidget {
+  AddContactPage({Key? key}) : super(key: key);
+
+  @override
+  _AddContactPageState createState() => _AddContactPageState();
+}
+
+class _AddContactPageState extends State<AddContactPage> {
+  final _nameController = TextEditingController();
+  final _surnameController = TextEditingController();
+  ContactOperations contactOperations = ContactOperations();
+  CategoryOperations categoryOperations = CategoryOperations();
+  Category _selectedCategory;
+
+  callback(selectedCategory) {
+    setState(() {
+      _selectedCategory = selectedCategory;
+      print(_selectedCategory.name);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SQFLite Tutorial'),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushReplacementNamed('/homePage');
+          },
+          child: Icon(
+            Icons.arrow_back,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Name'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: _surnameController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Surname'),
+              ),
+            ),
+            FutureBuilder<List<Category>>(
+              future: categoryOperations.getAllCategories(),
+              builder: (context, snapshot) {
+                return snapshot.hasData
+                    ? CategoriesDropDown(snapshot.data, callback)
+                    : Text('No categories');
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final contact = Contact(
+              name: _nameController.text,
+              surname: _surnameController.text,
+              category: _selectedCategory.id);
+          contactOperations.createContact(contact);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
